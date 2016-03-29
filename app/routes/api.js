1c3fa71d72b9237d4492d44b955bf1b2007ee06b
kimo5532 <shotbyabel@gmail.com>
2016-03-28 20:12:34 -0700

    Get all users request

--- app/routes/api.js
+++ app/routes/api.js
@@ -5,27 +5,43 @@ var User        = require('../models/user-models'),
     module.exports = function(app, express) {
 
       var api = express.Router();
+//create user route
 
-      api.post('/signup', function(req, res) {
+  api.post('/signup', function(req, res) {
 
-        var user = new User({
-          name: req.body.name,
-          username: req.body.username,
-          password: req.body.password
-        });
+    var user = new User({
+      name: req.body.name,
+      username: req.body.username,
+      password: req.body.password
+    });
 
-        user.save(function(err) {
-          if(err) {
-            res.send(err);
-            return;
-          }
+    user.save(function(err) {
+      if (err) {
+        res.send(err);
+        return;
+      }
 
-          res.json({ message: "A new user has been created!"});
-
-        });
-
+      res.json({
+        message: "A new user has been created!"
       });
+
+    });
+
+  });
+
+//get all users route
+  api.get('/users', function(req, res) {
+    User.find({}, function(err, users) {
+      if (err) {
+        res.send(err);
+        return;
+      }
+
+      res.json(users);
+    }); //mongoose find method
+  });
 
       return api
 
     }
+    
