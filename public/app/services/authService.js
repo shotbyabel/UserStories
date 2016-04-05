48c42c77760aeb3e53c0870dac0e78959e4a7820
kimo5532 <shotbyabel@gmail.com>
2016-04-04 21:26:00 -0700

    Create AuthToken factory and added nethods for localStorage

--- public/app/services/authService.js
+++ public/app/services/authService.js
@@ -1,51 +1,82 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular.module('authService', []);
-  .factory('Auth', function($http, $q, AuthToken) {
+    angular.module('authService', []);
+    // A U T H  F A C T O R Y  
+    .factory('Auth', function($http, $q, AuthToken) {
 
-    var authFactory = {};
+      var authFactory = {};
 
-    authFactory.login = function(username, password) { //get API from api.post
+      authFactory.login = function(username, password) { //get API from api.post
 
-        return $http.post('/api/login', {
-            username: username,
-            password: password
-          })
-          .success(function(data) {
-            AuthToken.setToken(data.token);
+          return $http.post('/api/login', {
+              username: username,
+              password: password
+            })
+            .success(function(data) {
+              AuthToken.setToken(data.token);
 
-            return data;
+              return data;
 
-          })
+            })
+
+        }
+        //logout - clear token
+      authFactory.logout = function() {
+        AuthToken.setToken();
+      }
+
+      //check if user is logged in
+      authFactory.isLoggedIN = function() {
+        if (AuthToken.getToken())
+          return true;
+
+        else
+          return false
+      }
+
+      //get all user info...
+      authFactory.getUser = function() {
+        if (AuthToken.getToken())
+          return $http.get('/api/me');
+
+        else
+          return $q.reject({
+            message: "User has no token!"
+          });
+      }
+
+    })
+
+    // T O K E N   F A C T O R Y    
+    .factory('AuthToken', function($window) {
+
+      var authTokenFactory = {}
+
+      //get token from the browser
+      authTokenFactory.getToken = function() {
+        return $window.localStorage.getItem('token');
 
       }
-      //logout - clear token
-    authFactory.logout = function() {
-      AuthToken.setToken();
-    }
+      //store token
+      authTokenFactory.setToken = function(token) {
 
-    //check if user is logged in
-    authFactory.isLoggedIN = function() {
-      if (AuthToken.getToken())
-        return true;
+        if (token)
+          $window.localStorage.setItem('token', token);
 
-      else
-        return false
-    }
+        else
 
-    //get all user info...
-    authFactory.getUser = function() {
-      if (AuthToken.getToken())
-        return $http.get('/api/me');
+          $window.localStorage.removeItem('token');
+      }
 
-      else
-        return $q.reject({
-          message: "User has no token!"
-        });
-    }
 
-  })
+    })
+
+
+
+
+
+
 
   /////
 })();
